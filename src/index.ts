55740d761336b9ed90d62d33119a39a45bee47bb
RufixDuke <rufixduke01@gmail.com>
2022-06-15 08:23:20 +0100

    adding it into the local storage

--- src/index.ts
+++ src/index.ts
@@ -34,10 +34,15 @@ function addListItem(task: Task) {
     const checkbox = document.createElement("input");
     checkbox.addEventListener("change", () => {
         task.completed = checkbox.checked;
+        saveTasks();
     });
     checkbox.type = "checkbox";
     checkbox.checked = task.completed;
     label.append(checkbox, task.title);
     item.append(label);
     list?.append(item);
+}
+
+function saveTasks() {
+    localStorage.setItem("TASKS", JSON.stringify(tasks));
 }
