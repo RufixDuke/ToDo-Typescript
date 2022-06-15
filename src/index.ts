946168e503a895f5eca8d3d743161885c0523ec0
RufixDuke <rufixduke01@gmail.com>
2022-06-15 08:07:00 +0100

    changes in the uuidV4 types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
-// import { v4 as uuidV4 } from "uuid";
+import { v4 as uuidV4 } from "uuid";
 
-// console.log(uuidV4);
+console.log(uuidV4);
 
 const list = document.querySelector("#list") as HTMLUListElement;
-const form = document.querySelector("#new-task-form") as HTMLFormElement;
+const form = document.querySelector("#new-task-form") as HTMLFormElement | null;
 const input = document.querySelector("#new-task-title") as HTMLInputElement;
+
+form?.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if (input?.value == "" || input?.value == null) return;
+
+    const task = {
+        id: uuidV4(),
+        title: input.value,
+        completed: false,
+        createdAt: new Date(),
+    };
+});
